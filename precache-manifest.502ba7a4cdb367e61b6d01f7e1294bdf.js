81764025414431e17ebce1aa659a3933130d982a
dci <wafi.amer.17@gmail.com>
2020-07-22 14:42:21 +0200

    Updates

---
+++ precache-manifest.502ba7a4cdb367e61b6d01f7e1294bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07be466dd9d54d1d063adf6b877188d",
+    "url": "/CocktailMaster/index.html"
+  },
+  {
+    "revision": "56afe94afab8c55d57b3",
+    "url": "/CocktailMaster/static/css/main.434c06be.chunk.css"
+  },
+  {
+    "revision": "2510c77613de1dfd9b94",
+    "url": "/CocktailMaster/static/js/2.1fbb4bf0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CocktailMaster/static/js/2.1fbb4bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56afe94afab8c55d57b3",
+    "url": "/CocktailMaster/static/js/main.aa32fc4b.chunk.js"
+  },
+  {
+    "revision": "a0fdbf714ff3a80a302a",
+    "url": "/CocktailMaster/static/js/runtime-main.07126e6c.js"
+  },
+  {
+    "revision": "1b35ed3a9f6ed3e9f77407087e568abb",
+    "url": "/CocktailMaster/static/media/multicolor-glass-cocktail-drinks-black-background-hd-wallpapers.1b35ed3a.jpg"
+  }
+]);
